31d63e878a4014a645092dbaed8604545bf25da0
Francisca999-tina <68696276+Francisca999-tina@users.noreply.github.com>
2021-01-08 16:55:41 -0300

    add interval

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const dino = Document.querySelector('.dino');
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        jump();
+    }
+}
+
+function jump() {
+    let position = 0;
+    
+    let upInterval = setInterval(() => {
+        if (position > = 150){
+            clearInterval (upInterval);
+            
+            // Descendo
+            let downInterval = setInterval (() => {
+                
+
+            }, 20);
+        } else {
+        // Subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
